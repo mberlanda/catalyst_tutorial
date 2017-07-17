d418a3cf28dc3ca676b68623fc956f7279cc2d9d
Mauro Berlanda <mauro.berlanda@mecglobal.com>
2017-07-17 18:52:27 +0200

    Basic CRUD | Add a Delete Action to the Controller

--- lib/MyApp/Controller/Books.pm
+++ lib/MyApp/Controller/Books.pm
@@ -142,6 +142,49 @@ sub form_create_do :Chained('base') :PathPart('form_create_do') :Args(0) {
               template => 'books/create_done.tt2');
 }
 
+=head2 delete
+
+Delete a book
+
+=cut
+
+sub delete :Chained('object') :PathPart('delete') :Args(0) {
+    my ($self, $c) = @_;
+
+    # Use the book object saved by 'object' and delete it along
+    # with related 'book_author' entries
+    $c->stash->{object}->delete;
+
+    # Set a status message to be displayed at the top of the view
+    $c->stash->{status_msg} = "Book deleted.";
+
+    # Forward to the list action/method in this controller
+    $c->forward('list');
+}
+
+=head2 object
+
+Fetch the specified book object based on the book ID and store
+it in the stash
+
+=cut
+
+sub object :Chained('base') :PathPart('id') :CaptureArgs(1) {
+    # $id = primary key of book to delete
+    my ($self, $c, $id) = @_;
+
+    # Find the book object and store it in the stash
+    $c->stash(object => $c->stash->{resultset}->find($id));
+
+    # Make sure the lookup was successful.  You would probably
+    # want to do something like this in a real app:
+    #   $c->detach('/error_404') if !$c->stash->{object};
+    die "Book $id not found!" if !$c->stash->{object};
+
+    # Print a message to the debug log
+    $c->log->debug("*** INSIDE OBJECT METHOD for obj id=$id ***");
+}
+
 =head2 base
 
 Can place common logic to start chained dispatch here
